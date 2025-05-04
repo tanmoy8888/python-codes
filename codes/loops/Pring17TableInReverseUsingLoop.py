d696d3d295d07940d9fa516e53bf6e92447d9074
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-04 13:23:33 +0530

    table of 17

---
+++ codes/loops/Pring17TableInReverseUsingLoop.py
@@ -0,0 +1,7 @@
+def main():
+    table_of = 17
+    for i in range(11,1,-1):
+        print(f'value of i is --> {i*17}')
+
+if __name__ == "__main__":
+    main()
---
+++ codes/loops/Pring17TableUsingLoop.py
@@ -0,0 +1,7 @@
+def main():
+    table_of = 17
+    for i in range(1,11):
+        print(f'value of i is --> {i*17}')
+
+if __name__ == "__main__":
+    main()
