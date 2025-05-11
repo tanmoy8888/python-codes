f5e4d9d1739b318bc8e2278b5458e2d8272f48a6
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-11 13:15:03 +0530

    function code

---
+++ codes/function/EvenOrOdd.py
@@ -0,0 +1,12 @@
+def even_or_odd():
+    a = int(input("Enter the number , "))
+    if a%2 == 0:
+        print("Given number is even")
+    else:
+        print("Given number is odd")
+def main():
+    even_or_odd()
+
+
+if __name__ == "__main__":
+    main()
