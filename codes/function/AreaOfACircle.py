28754e80fcbcf9c39003fb82c387a381c1308d87
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-11 13:29:02 +0530

    function code

---
+++ codes/function/AreaOfACircle.py
@@ -0,0 +1,10 @@
+def area_of_a_circle():
+    r = int(input("Enter the radios of a circle , "))
+    pi = 3.142
+    print("Area of a circle is  " , pi*r*r)
+def main():
+    area_of_a_circle()
+
+
+if __name__ == "__main__":
+    main()
---
+++ codes/function/LengthOfAString.py
@@ -0,0 +1,12 @@
+def length_of_a_string():
+    s = input("Enter A string , ")
+    length = 0
+    for i in s:
+        length = length+1
+    print(length)
+def main():
+    length_of_a_string()
+
+
+if __name__ == "__main__":
+    main()
