8dfbc105ce2f027a3df653049660fcb77d0b63ce
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-11 13:19:04 +0530

    function code

---
+++ codes/function/Multiplication.py
@@ -0,0 +1,10 @@
+def multiplication():
+    a = int(input("Enter the number , "))
+    for i in range(1,11):
+        print(f"{i} is :: " , i*a)
+def main():
+    multiplication()
+
+
+if __name__ == "__main__":
+    main()
