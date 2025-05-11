2f5da4a4c881e0df0cea58c9f878f4559de06a0b
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-11 13:11:42 +0530

    function code

---
+++ codes/function/MaxNumber.py
@@ -0,0 +1,13 @@
+def max_number():
+    a = int(input("Enter first number , "))
+    b = int(input("Enter second number , "))
+    if a > b:
+        print("a is greater than b")
+    else:
+        print("b is greater than a")
+def main():
+    max_number()
+
+
+if __name__ == "__main__":
+    main()
