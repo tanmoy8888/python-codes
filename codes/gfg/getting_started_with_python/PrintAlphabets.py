6efb22f8ba00f746bb4dcd14332e3722f7914bf1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-13 22:58:00 +0530

    function code

---
+++ codes/gfg/getting_started_with_python/PrintAlphabets.py
@@ -0,0 +1,9 @@
+# Given two characters, c1 and c2, print all the alphabets starting from c1 to c2, separated by a space, in a single line.
+# Do not print a newline yourself, as it will be handled by the driver code.
+
+def alphabets(c1,c2):
+
+def main():
+ alphabets('a','z')
+if __name__ == "__main__":
+    main()
