dbbf799ac9ae7781538aa47aea092cc050701ce1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-13 22:54:06 +0530

    function code

--- codes/gfg/getting_started_with_python/CapitalizeLetter.py
+++ codes/gfg/getting_started_with_python/CapitalizeLetter.py
@@ -3,3 +3,12 @@
 # 1. Capitalize the first letter and print.
 #
 # 2. Convert the whole string to uppercase and print.
+
+def changeCase(s):
+ print(s.capitalize())
+ print(s.upper())
+
+def main():
+ changeCase("Hello World")
+if __name__ == "__main__":
+    main()
