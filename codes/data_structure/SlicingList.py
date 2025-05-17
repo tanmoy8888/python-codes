4da825bb28c83605965c17df40d859f573a6afda
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-17 12:53:53 +0530

    python ds

---
+++ codes/data_structure/SlicingList.py
@@ -0,0 +1,13 @@
+def slicing_list():
+    list = []
+    for i in range(1, 51):
+            list.append(i)
+    print(list[0:5])
+
+
+def main():
+    slicing_list()
+
+
+if __name__ == "__main__":
+    main()
