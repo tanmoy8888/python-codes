2846aa8820f94a9d0680eef0c0fd475639e705ca
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-17 13:17:24 +0530

    python ds

---
+++ codes/data_structure/ReverseStringList.py
@@ -0,0 +1,11 @@
+def list_print():
+    string_list = ["apple", "banana", "cherry"]
+    string_list.reverse()
+    print(string_list)
+
+def main():
+    list_print()
+
+
+if __name__ == "__main__":
+    main()
