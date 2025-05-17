eb317a883d0023498acab680676c436847c8a365
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-17 13:30:16 +0530

    python ds

--- codes/data_structure/CheckInList.py
+++ codes/data_structure/CheckInList.py
@@ -1,11 +1,13 @@
 def list_print():
     names = ["Aarav", "Aditi", "Arjun", "Diya", "Kabir", "Kiara", "Rohan", "Sanya", "Vedant", "Zara", "Aryan", "Ananya", "Krishna", "Priya", "Vikram"]
+    found = False
     for s in names:
         if s == "Dhawal":
             print("Name is present")
+            found = True
             break
-        else:
-            print("Name is not present")
+    if not found:
+        print("Name is not present")
 
 def main():
     list_print()
