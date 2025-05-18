a2415e72826c2493ed553dfe44a6c6cf0e1a2be4
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-18 12:40:40 +0530

    python list

---
+++ codes/data_structure/Calculate.py
@@ -0,0 +1,11 @@
+def calculate():
+   student1= [56,78,45,98,100,33]
+   print(sum(student1))
+   print("Avg",sum(student1)/6)
+   print(max(student1))
+   print(min(student1))
+def main():
+    calculate()
+
+if __name__ == "__main__":
+    main()
