ef359852e6e4e3d69ffedfd0924361d1b621578c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-18 13:46:27 +0530

    python tuples, enumurate

---
+++ codes/data_structure/tuples/Ennumurate.py
@@ -0,0 +1,9 @@
+def enumerate_check():
+   tuple_pack =[1,2,3,4,5]
+   for i,obj in enumerate(tuple_pack):
+       print((i,obj))
+def main():
+    enumerate_check()
+
+if __name__ == "__main__":
+    main()
