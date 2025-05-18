b10026985be773e0b027e103704c11f6361f4fd9
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-18 13:20:30 +0530

    python tuples

---
+++ codes/data_structure/tuples/UnpackingTuple.py
@@ -0,0 +1,12 @@
+def unpacking():
+   tuple_pack =[1,2,"hello",True]
+   a,b,c,d = tuple_pack
+   print(a)
+   print(b)
+   print(c)
+   print(d)
+def main():
+    unpacking()
+
+if __name__ == "__main__":
+    main()
