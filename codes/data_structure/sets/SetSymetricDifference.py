c86a640e27350f4461e259097604efba01f650ca
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-24 12:54:43 +0530

    python sets

--- codes/data_structure/sets/SetSymetricDifference.py
+++ codes/data_structure/sets/SetSymetricDifference.py
@@ -1,6 +1,7 @@
 def check_set():
    my_set_1 = {1,2,3,4,4,5,5}
    my_set_2 = {5,6,6,7,8,9,10}
+   # Symmetric Difference returns all the unique elements from both sets
    print(my_set_1.symmetric_difference(my_set_2))
 def main():
     check_set()
