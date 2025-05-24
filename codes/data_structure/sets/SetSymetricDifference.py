c5999e1026bb4e2353e91eb628ce1d83f7f00c61
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-24 12:53:45 +0530

    python sets

---
+++ codes/data_structure/sets/SetSymetricDifference.py
@@ -0,0 +1,9 @@
+def check_set():
+   my_set_1 = {1,2,3,4,4,5,5}
+   my_set_2 = {5,6,6,7,8,9,10}
+   print(my_set_1.symmetric_difference(my_set_2))
+def main():
+    check_set()
+
+if __name__ == "__main__":
+    main()
