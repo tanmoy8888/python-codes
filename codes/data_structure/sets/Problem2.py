d669e782ed93820bab36cf5e03bb2dfa1500fdd0
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-24 13:12:06 +0530

    python sets

---
+++ codes/data_structure/sets/Problem2.py
@@ -0,0 +1,11 @@
+def check_set():
+   text= "hi everyone i am from gfg data science , data analytics , data engineering , i am teaching in this batch , thanks everyone"
+   my_set = set()
+   for s in text.split(" "):
+       my_set.add(s)
+   print(my_set)
+def main():
+    check_set()
+
+if __name__ == "__main__":
+    main()
