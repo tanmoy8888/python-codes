c023348daf59c116f40e5036d26976914c56ebe7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-24 12:33:50 +0530

    python sets

---
+++ codes/data_structure/sets/SetAddElements.py
@@ -0,0 +1,12 @@
+def check_set():
+   my_set_1 = {1,2,3,4,5,5}
+   print(my_set_1)
+   my_set_1.add(6)
+   print(my_set_1)
+   my_set_1.remove(6)
+   print(my_set_1)
+def main():
+    check_set()
+
+if __name__ == "__main__":
+    main()
