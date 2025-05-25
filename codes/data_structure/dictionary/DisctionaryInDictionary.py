df9078cfdf524e99e678c471715d82eb1b3c0540
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-05-25 12:45:56 +0530

    python dictionary

---
+++ codes/data_structure/dictionary/DisctionaryInDictionary.py
@@ -0,0 +1,13 @@
+def check_set():
+    students = {
+        "student1" : {"name" : "Amit" , "age" : 10},
+        "student2" : {"name" : "Rahul" , "age" : 11},
+        "student3" : {"name" : "Rakesh" , "age" : 12}
+    }
+    print(students)
+def main():
+    check_set()
+
+
+if __name__ == "__main__":
+    main()
