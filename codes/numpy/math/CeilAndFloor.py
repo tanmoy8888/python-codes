74dec8ab53ba9314d76864286939fce0f3031f5d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 12:43:21 +0530

    math package

---
+++ codes/numpy/math/CeilAndFloor.py
@@ -0,0 +1,7 @@
+import math
+def main():
+    x = 5
+    print(math.ceil(x))
+    print(math.floor(x))
+if __name__ == "__main__":
+    main()
---
+++ codes/numpy/math/Math.py
@@ -0,0 +1,9 @@
+import math
+def main():
+    x = 5
+    print(math.exp(x))
+    print(math.sin(x))
+    print(math.sqrt(x))
+    print(math.log10(x))
+if __name__ == "__main__":
+    main()
