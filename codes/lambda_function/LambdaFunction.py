d8d8d667539c2c1a6cc7ec21fd80c9083dfc3e0f
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 13:28:52 +0530

    lambda function

--- codes/lambda_function/LambdaFunction.py
+++ codes/lambda_function/LambdaFunction.py
@@ -1,3 +1,5 @@
+import math
+
 
 def main():
     print(add(2, 3))
@@ -5,6 +7,8 @@ def main():
     # Require a lambda keyword
     addition = lambda a, b: a + b
     print(addition(4,4))
+    sqrt = lambda x:math.sqrt(x)
+    print(sqrt(16))
 def add(a,b):
     return a+b
 if __name__ == "__main__":
