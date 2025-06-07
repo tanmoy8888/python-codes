92295d2789983ea3547d0cb76cd07e0ff032eb3f
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 13:34:08 +0530

    lambda function

--- codes/lambda_function/LambdaFunction.py
+++ codes/lambda_function/LambdaFunction.py
@@ -1,5 +1,7 @@
 import math
 
+from codes.function.EvenOrOdd import even_or_odd
+
 
 def main():
     print(add(2, 3))
@@ -8,7 +10,9 @@ def main():
     addition = lambda a, b: a + b
     print(addition(4,4))
     sqrt = lambda x:math.sqrt(x)
+    even_or_odd = lambda x: x%2==0
     print(sqrt(16))
+    print("Checking even or odd :: ",even_or_odd(6))
 def add(a,b):
     return a+b
 if __name__ == "__main__":
