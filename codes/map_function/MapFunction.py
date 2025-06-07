54944c39bc60372787ecf874b98c1ccf18961e29
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 13:45:20 +0530

    lambda function

--- codes/map_function/MapFunction.py
+++ codes/map_function/MapFunction.py
@@ -1,6 +1,10 @@
 def main():
     lst = [1,2,3,4,5]
     print(list(map(lambda x: x * x, lst)))
+    # Addition of two list
+    list_1 = [1,2,3,4]
+    list_2 = [5,6,7,8]
+    print(list(map(lambda x, y: x + y, list_1, list_2)))
 def add(a,b):
     return a+b
 if __name__ == "__main__":
