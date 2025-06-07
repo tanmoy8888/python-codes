c4e1e4d60128a4120f80ad37a7cd5b0d901ed383
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 13:14:52 +0530

    date time package

--- codes/numpy/date_time/DateTime.py
+++ codes/numpy/date_time/DateTime.py
@@ -5,5 +5,6 @@ def main():
     print(datetime.datetime(2025,6,7,1,8,10))
     # Format date time
     print(datetime.datetime.now().strftime("%d:%m:%y:%H:%M:%S"))
+    print(datetime.datetime.now().strftime("%d:%m:%y"))
 if __name__ == "__main__":
     main()
