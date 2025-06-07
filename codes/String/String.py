36debfae295273753f904cbb619ee0e212a85134
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 13:19:14 +0530

    string package

--- codes/String/String.py
+++ codes/String/String.py
@@ -2,5 +2,10 @@ import string
 
 def main():
     print(string.digits)
+    x = "data science"
+    print(x.upper())
+    print(x.lower())
+    print(x.capitalize())
+    print(x.title())
 if __name__ == "__main__":
     main()
