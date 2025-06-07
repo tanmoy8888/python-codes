a3d7a576ed4d1eaf9757a0ae17c4aefe5935ac9a
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-07 12:35:38 +0530

    numpy code

--- codes/numpy/FirstNP.py
+++ codes/numpy/FirstNP.py
@@ -3,9 +3,13 @@ def main():
     data = np.array([1,2,3,4,00,5])
     min_val = np.min(data)
     max_val = np.max(data)
+    std_val = np.std(data)
     normalization_array = (data-min_val)/(max_val-min_val)
+    normalization_array_std_deviation = (data-min_val)/std_val
     print("min_val" , min_val)
     print("max_val" , max_val)
+    print("std_val" , std_val)
     print("normalization_array" , normalization_array)
+    print("normalization_array_std_deviation" , normalization_array_std_deviation)
 if __name__ == "__main__":
     main()
