a23b5f0524a5c5591ac8f91c6fe25ca50f5f0453
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-08 13:55:48 +0530

    pandas code

--- codes/dataset/Dataset1.py
+++ codes/dataset/Dataset1.py
@@ -5,6 +5,10 @@ def main():
   #printing specific value
   df1 = df[df['Region'] == 'Europe']
   print(df1)
+  # sorting on a specific column
+  df2 = df.sort_values(by='Total Revenue' , ascending=False)
+  print("----------Printing in sorted order-------------")
+  print(df2)
 
 if __name__ == "__main__":
     main()
