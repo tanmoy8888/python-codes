4e8f7c11f33b2080730909b9d04a4dda1acc149e
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-08 12:26:35 +0530

    pandas code

--- codes/pandas/Series.py
+++ codes/pandas/Series.py
@@ -3,7 +3,12 @@ def main():
     data = [1,2,3,4,5]
     # In pandas, it automatically creates an index in the data.
     # In Series of Pandas it can generate one dimensional series not multidimensional like numpy
-    series = pd.Series(data)
-    print(series)
+    series_1 = pd.Series(data)
+    print(series_1)
+    # data with index in pandas series
+    data = [1,2,3]
+    index = ['a','b','c']
+    series_2 = pd.Series(data, index)
+    print(series_2)
 if __name__ == "__main__":
     main()
