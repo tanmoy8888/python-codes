e03cfe1e83a8341e3628980b29801ba12c6716b5
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-08 13:27:31 +0530

    pandas code

--- codes/dataset/DataSet.py
+++ codes/dataset/DataSet.py
@@ -3,6 +3,11 @@ import pandas as pd
 def main():
   df = pd.read_csv('sales_data.csv')
   print(df)
+  print("--------------Printing first five rows----------------")
+  print(df.head())
+
+  print("--------------Printing last five rows----------------")
+  print(df.tail())
 
 if __name__ == "__main__":
     main()
