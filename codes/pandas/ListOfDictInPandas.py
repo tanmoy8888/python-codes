4b910748ffcf99c16e9ac6478bc7c67666027af5
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-08 12:39:30 +0530

    pandas code

---
+++ codes/pandas/ListOfDictInPandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# Pandas dataframe can be used for multidimensional data
+def main():
+    # dictionary
+    data = [
+        {'Name':'Amal','Age':20,'Address':'Noida'},
+        {'Name':'John','Age':21,'Address':'New York'},
+        {'Name':'Rahul','Age':22,'Address':'Mumbai'},
+    ]
+    df = pd.DataFrame(data)
+    print(df)
+
+if __name__ == "__main__":
+    main()
