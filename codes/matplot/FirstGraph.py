a2585debfa95cad7ebd9f7ca214b53d82f2876ff
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:13:35 +0530

    pandas code

---
+++ codes/matplot/FirstGraph.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  x = [1,2,3,4]
+  y = [10,20,30,40]
+  plt.plot(x,y)
+  plt.show()
+
+if __name__ == "__main__":
+    main()
