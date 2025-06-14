467e63e52f0b81f8713c4f48c19a24be46fb6a0d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:24:14 +0530

    matplot libs

---
+++ codes/matplot/PreDefinedStyle.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  # Available styles for chart/graphs
+  print(plt.style.available)
+
+if __name__ == "__main__":
+    main()
