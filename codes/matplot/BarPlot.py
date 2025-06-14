46353984bee9ea40578dc4c20b728fc6104280e9
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:32:04 +0530

    matplot libs

--- codes/matplot/BarPlot.py
+++ codes/matplot/BarPlot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def main():
   x = ['A','B','C','D']
   y = [10,20,30,40]
-  plt.bar(x,y,color='c')
+  plt.bar(x,y,color='c',linewidth=3)
   plt.xlabel("X Values")
   plt.ylabel("Y Values")
   plt.title("My First Line Plot")
