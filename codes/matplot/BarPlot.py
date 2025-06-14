e1d03d83d27d860dfc6abfe22144b129d317a393
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:29:13 +0530

    matplot libs

---
+++ codes/matplot/BarPlot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  x = ['A','B','C','D']
+  y = [10,20,30,40]
+  plt.bar(x,y,color='c')
+  plt.xlabel("X Values")
+  plt.ylabel("Y Values")
+  plt.title("My First Line Plot")
+  plt.show()
+
+if __name__ == "__main__":
+    main()
