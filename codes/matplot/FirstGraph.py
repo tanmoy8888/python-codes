7f72aeec9343956ae705b88b481b84ed22670772
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:21:06 +0530

    matplot libs

--- codes/matplot/FirstGraph.py
+++ codes/matplot/FirstGraph.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 def main():
   x = [1,2,3,4]
   y = [10,20,30,40]
-  plt.figure(figsize=(5,6))
+  plt.figure(figsize=(6,6))
   # alpha = transparency
   plt.plot(x,y,color='c',linewidth=3,marker='o',markerfacecolor='blue',alpha=1)
+  # For adding grid
+  plt.grid()
   plt.xlabel("X Values")
   plt.ylabel("Y Values")
   plt.title("My First Line Plot")
