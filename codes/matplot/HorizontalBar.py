8223bbf6adc04b65910732e824d4216e4bacda00
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-14 13:33:36 +0530

    matplot libs

---
+++ codes/matplot/HorizontalBar.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  x = ['A','B','C','D']
+  y = [10,20,30,40]
+  # Here instead plt.bar , use plt.barh
+  plt.barh(x,y,color='c',linewidth=3)
+  plt.xlabel("X Values")
+  plt.ylabel("Y Values")
+  plt.title("My First Line Plot")
+  plt.show()
+
+if __name__ == "__main__":
+    main()
