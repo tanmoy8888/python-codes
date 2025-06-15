097acf8ffbbfca00270c8afef54ead77f5eea8fe
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 13:17:21 +0530

    seaborn box plot code

--- codes/seaborn/Seaborn.py
+++ codes/seaborn/Seaborn.py
@@ -5,7 +5,10 @@ import seaborn as sns
 # Pandas dataset
 def main():
   df = pd.read_csv("../heatmap/sales_data.csv")
-  sns.histplot(df['Total Revenue'],bins=20)
+  #sns.histplot(df['Total Revenue'],bins=20)
+  plt.figure(figsize=(10,5))
+  # Box plot
+  sns.boxplot(x='Product Category',y='Total Revenue',data=df)
   plt.show()
 if __name__ == "__main__":
     main()
