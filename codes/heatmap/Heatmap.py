d75d98cba3476f563a8118f7a074e19aba99c9cf
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 12:24:42 +0530

    heatmap

--- codes/heatmap/Heatmap.py
+++ codes/heatmap/Heatmap.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 # Pandas dataset
 def main():
   x = np.random.rand(4,4)
+  ## Heatmap is used for correlation
+
   #plt.imshow(x,cmap = 'hot',)
   plt.imshow(x,cmap = 'hot',interpolation='bicubic')
   plt.colorbar()
