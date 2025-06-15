ccd4f68b0520f061657abcfcc1fafb5e54433dd6
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 13:04:49 +0530

    data visualization , pie chart

---
+++ codes/data_visualization/UsingPieChart.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  df = pd.read_csv("../heatmap/sales_data.csv")
+  data = df.groupby("Product Category")["Total Revenue"].sum()
+  # Revenue over time
+  df['Date'] = pd.to_datetime(df['Date'])
+  plt.figure(figsize=(10,5))
+  #plt.plot(df['Date'],df['Total Revenue'], marker='o')
+  #plt.hist(df['Total Revenue'], bins=25 , edgecolor='black')
+  plt.pie(data,labels=data.index ,colors=['b','c','yellow'])
+  plt.show()
+if __name__ == "__main__":
+    main()
