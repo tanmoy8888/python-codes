8b9cf02c001a2bfc2119132787a3d21fae5a9675
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 12:23:38 +0530

    heatmap

--- codes/heatmap/Heatmap.py
+++ codes/heatmap/Heatmap.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 # Pandas dataset
 def main():
   x = np.random.rand(4,4)
-  plt.imshow(x,cmap = 'hot',)
+  #plt.imshow(x,cmap = 'hot',)
+  plt.imshow(x,cmap = 'hot',interpolation='bicubic')
   plt.colorbar()
   plt.show()
 
