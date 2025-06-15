8abf90006604d2423804af388bad0e67e909faae
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 13:24:37 +0530

    seaborn bar plot code

---
+++ codes/seaborn/exercrise/PlotForUnitSoldByPaymentMethod.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Pandas dataset
+def main():
+  df = pd.read_csv("../../heatmap/sales_data.csv")
+  #sns.histplot(df['Total Revenue'],bins=20)
+  plt.figure(figsize=(10,5))
+  # Bar plot
+  #sns.boxplot(x='Product Category',y='Total Revenue',data=df)
+  sns.barplot(x='Payment Method',y='Total Revenue',data=df)
+  plt.show()
+if __name__ == "__main__":
+    main()
