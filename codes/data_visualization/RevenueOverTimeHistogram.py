bc4dc2a314c4ea7630b9dcb05d4c135e52ae9bce
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 12:55:40 +0530

    heatmap

---
+++ codes/data_visualization/RevenueOverTimeHistogram.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# Pandas dataset
+def main():
+  df = pd.read_csv("../heatmap/sales_data.csv")
+  data = df.groupby("Product Category")["Total Revenue"].sum()
+  # Revenue over time
+  df['Date'] = pd.to_datetime(df['Date'])
+  plt.figure(figsize=(10,5))
+  #plt.plot(df['Date'],df['Total Revenue'], marker='o')
+  plt.hist(df['Total Revenue'], bins=25)
+  plt.xlabel('Total Revenue')
+  plt.ylabel('Frequency')
+  plt.title('Distribution of Total Revenue')
+  plt.show()
+  plt.show()
+if __name__ == "__main__":
+    main()
