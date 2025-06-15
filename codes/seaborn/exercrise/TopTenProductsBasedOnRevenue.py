100643612c86d2ee9fea3df91b508d021d2b94e4
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-15 13:38:36 +0530

    seaborn bar plot code

--- codes/seaborn/exercrise/TopTenProductsBasedOnRevenue.py
+++ codes/seaborn/exercrise/TopTenProductsBasedOnRevenue.py
@@ -6,10 +6,8 @@ import seaborn as sns
 def main():
   df = pd.read_csv("../../heatmap/sales_data.csv")
   #sns.histplot(df['Total Revenue'],bins=20)
-  plt.figure(figsize=(10,5))
-  #sns.boxplot(x='Product Category',y='Total Revenue',data=df)
-  data = df.groupby("Product Name")["Total Revenue"].sum().sort_values(ascending=False).reset_index().head(10)
-  sns.barplot(x='Product Name',y='Total Revenue',data=df)
+  product_name_data=df.groupby("Product Name")["Total Revenue"].sum().sort_values(ascending=False).reset_index().head(10)
+  sns.barplot(x="Product Name",y="Total Revenue",data=product_name_data)
   plt.show()
 if __name__ == "__main__":
     main()
