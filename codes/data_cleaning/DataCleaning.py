3b75a00d165edf05fd7e053a6ac05a60dcb250c3
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-06-22 12:27:23 +0530

    data cleaning

--- codes/data_cleaning/DataCleaning.py
+++ codes/data_cleaning/DataCleaning.py
@@ -4,6 +4,7 @@ import numpy as np
 def main():
   df = pd.read_excel("../../data/flight_price.xlsx")
   print(df)
-  df.dropna()
+  df.dropna(subset=['Route','Total_Stops'],inplace=True)
+  print(df.isnull().sum())
 if __name__ == "__main__":
     main()
